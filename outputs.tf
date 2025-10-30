40c2fb0d916a99b334ac601115297883f5e38945
Viacheslav Kaliak <trinidaa@gmial.com>
2025-10-30 10:47:08 +0100

    solution-1.1

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,11 @@
 output "subnet_id" {
     description = "ID of the VPC subnet, deployed by the module"
     value       = "aws_subnet.main.id"
+    sensitive = true
 }
 
 output "security_group_id" {
     description = "ID of the security group, deployed by the module"
     value       = "aws_security_group.allow_tls.id"
+    sensitive = true
 }
