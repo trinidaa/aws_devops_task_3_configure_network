60dab7e323633604a187674e988f60daf4496b25
Viacheslav Kaliak <trinidaa@gmial.com>
2025-10-30 10:41:31 +0100

    solution-1.1

--- main.tf
+++ main.tf
@@ -1,4 +1,84 @@
-# Add your code here: 
+resource "aws_subnet" "main" {
+  vpc_id     = var.vpc_id
+  cidr_block = "10.10.10.0/24"
+
+  tags = {
+    Name = "Grafana"
+  }
+}
+
+resource "aws_internet_gateway" "gw" {
+  vpc_id = var.vpc_id
+
+  tags = {
+    Name = "mate-aws-grafana-lab"
+  }
+}
+
+resource "aws_route_table" "route_table" {
+  vpc_id = var.vpc_id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+
+  tags = {
+    Name = "mate-aws-grafana-lab"
+  }
+}
+
+resource "aws_route_table_association" "public" {
+  subnet_id      = aws_subnet.main.id
+  route_table_id = aws_route_table.route_table.id
+}
+
+resource "aws_security_group" "allow_tls" {
+  name = "mate-aws-grafana-lab"
+  description = "allow tls traffic"
+  vpc_id = var.vpc_id
+
+  tags = {
+    Name = "mate-aws-grafana-lab"
+  }
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_http" {
+  security_group_id = aws_security_group.allow_tls.id
+
+  cidr_ipv4   = "0.0.0.0/0"
+  ip_protocol = "tcp"
+  from_port   = 80
+  to_port     = 80
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_https" {
+  security_group_id = aws_security_group.allow_tls.id
+
+  cidr_ipv4   = "0.0.0.0/0"
+  ip_protocol = "tcp"
+  from_port   = 443
+  to_port     = 443
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_ssh" {
+  security_group_id = aws_security_group.allow_tls.id
+
+  cidr_ipv4   = "45.137.78.18/32"
+  ip_protocol = "tcp"
+  from_port   = 22
+  to_port     = 22
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_grafana" {
+  security_group_id = aws_security_group.allow_tls.id
+
+  cidr_ipv4   = "0.0.0.0/0"
+  ip_protocol = "tcp"
+  from_port   = 3000
+  to_port     = 3000
+}
+# Add your code here:
 
 # 1. Create a subnet 
 # 2. Create an Internet Gateway and attach it to the vpc
@@ -6,8 +86,25 @@
 # 4. Create a Security Group and inbound rules 
 # 5. Uncommend (and update the value of security_group_id if required) outbound rule - it required 
 # to allow outbound traffic from your virtual machine: 
-# resource "aws_vpc_security_group_egress_rule" "allow_all_eggress" {
-#   security_group_id = aws_security_group.security_group.id
-#   cidr_ipv4   = "0.0.0.0/0"
-#   ip_protocol = -1
+resource "aws_vpc_security_group_egress_rule" "allow_all_egress" {
+  security_group_id = aws_security_group.allow_tls.id
+  cidr_ipv4   = "0.0.0.0/0"
+  ip_protocol = -1
+}
+
+# resource "aws_security_group_rule" "egress_https" {
+#   type        = "egress"
+#   protocol    = "tcp"
+#   from_port   = 443
+#   to_port     = 443
+#   cidr_blocks = ["0.0.0.0/0"]
+#   security_group_id = aws_security_group.allow_tls.id
+# }
+# resource "aws_security_group_rule" "egress_dns" {
+#   type        = "egress"
+#   protocol    = "udp"
+#   from_port   = 53
+#   to_port     = 53
+#   cidr_blocks = ["0.0.0.0/0"]
+#   security_group_id = aws_security_group.allow_tls.id
 # }
--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,9 @@
 output "subnet_id" {
     description = "ID of the VPC subnet, deployed by the module"
-    value       = ""
+    value       = "aws_subnet.main.id"
 }
 
 output "security_group_id" {
     description = "ID of the security group, deployed by the module"
-    value       = ""
+    value       = "aws_security_group.allow_tls.id"
 }
